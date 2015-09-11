70ed6db0557f1404dfcecafd36e3b5f553a750ed
chasee <duke.chase@gmail.com>
2015-09-11 09:00:40 -0600

    Add figures to reference in forum question.

---
+++ Question Reference/plot1a.R
@@ -0,0 +1,9 @@
+if(file.exists("household_power_consumption.txt")) {
+  a <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+  Time <- as.POSIXlt(paste(a$Date, a$Time), format = "%d/%m/%Y %T")
+  b <- cbind(Time, a[3:9])
+  power.data <- subset(b, subset = b$Time >= "2007-02-01 00:00:00" & b$Time < "2007-02-03 00:00:00")
+}
+png("plot1a.png")
+hist(as.numeric(power.data$Global_active_power), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", freq = TRUE, col = "red")
+dev.off()
